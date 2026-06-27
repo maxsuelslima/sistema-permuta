717038a113ac52a3a5d6d2679397c76a34a45bb4
Maxsuel Lima <maxsuelslima@gmail.com>
2026-06-27 12:50:22 -0300

    update.

--- src/app/constans/pjes/2026/pjesJulho.ts
+++ src/app/constans/pjes/2026/pjesJulho.ts
@@ -1,158 +1,208 @@
 import { PJES } from '@/app/types/PJES';
 const pjesJulhoSemMot = [
     {
-        dia: '16',
-        matricula: '725258-7',
-        turno: 'P',
-    },
-    {
-        dia: '18',
-        matricula: '725261-7',
-        turno: 'P1',
-    },
-    {
-        dia: '20',
-        matricula: '725261-7',
-        turno: 'P1',
-    },
-    {
-        dia: '28',
-        matricula: '725261-7',
-        turno: 'P1',
-    },
-    {
-        dia: '12',
-        matricula: '725261-7',
-        turno: 'P1',
-    },
-    {
-        dia: '10',
-        matricula: '725261-7',
-        turno: 'P1',
-    },
-    {
-        dia: '4',
-        matricula: '725261-7',
-        turno: 'P1',
-    },
-    {
         dia: '2',
         matricula: '725261-7',
         turno: 'P1',
     },
     {
-        dia: '12',
-        matricula: '725262-5',
+        dia: '6',
+        matricula: '707401-8',
+        turno: 'P1',
+    },
+    {
+        dia: '6',
+        matricula: '725261-7',
+        turno: 'P2',
+    },
+    {
+        dia: '2',
+        matricula: '707401-8',
         turno: 'P2',
     },
     {
         dia: '18',
+        matricula: '725261-7',
+        turno: 'P1',
+    },
+    {
+        dia: '22',
+        matricula: '725261-7',
+        turno: 'P1',
+    },
+    {
+        dia: '22',
+        matricula: '707401-8',
+        turno: 'P2',
+    },
+    {
+        dia: '18',
+        matricula: '707401-8',
+        turno: 'P2',
+    },
+    {
+        dia: '14',
+        matricula: '725262-5',
+        turno: 'P1',
+    },
+    {
+        dia: '14',
+        matricula: '725261-7',
+        turno: 'P2',
+    },
+    {
+        dia: '24',
+        matricula: '725261-7',
+        turno: 'P1',
+    },
+    {
+        dia: '24',
         matricula: '725262-5',
         turno: 'P2',
     },
     {
         dia: '17',
-        matricula: '725262-5',
-        turno: 'P',
+        matricula: '707401-8',
+        turno: 'P1',
     },
     {
-        dia: '20',
-        matricula: '725262-5',
-        turno: 'P2',
-    },
-    {
-        dia: '10',
+        dia: '17',
         matricula: '725262-5',
         turno: 'P2',
     },
     {
-        dia: '28',
-        matricula: '711045-6',
-        turno: 'P2',
-    },
-    {
-        dia: '24',
-        matricula: '711045-6',
-        turno: 'P',
-    },
-    {
-        dia: '21',
-        matricula: '711045-6',
+        dia: '30',
+        matricula: '725262-5',
         turno: 'P1',
     },
     {
-        dia: '25',
-        matricula: '711045-6',
-        turno: 'P1',
-    },
-    {
-        dia: '1',
-        matricula: '707401-8',
-        turno: 'P1',
-    },
-    {
-        dia: '1',
-        matricula: '711045-6',
-        turno: 'P2',
-    },
-    {
-        dia: '26',
-        matricula: '725261-7',
-        turno: 'P1',
-    },
-    {
-        dia: '26',
-        matricula: '707401-8',
-        turno: 'P2',
-    },
-    {
-        dia: '2',
-        matricula: '707401-8',
-        turno: 'P2',
-    },
-    {
-        dia: '25',
-        matricula: '707401-8',
-        turno: 'P2',
-    },
-    {
-        dia: '21',
+        dia: '30',
         matricula: '707401-8',
         turno: 'P2',
     },
     {
         dia: '4',
         matricula: '711045-6',
+        turno: 'P',
+    },
+    {
+        dia: '16',
+        matricula: '711045-6',
+        turno: 'P',
+    },
+    {
+        dia: '12',
+        matricula: '711045-6',
+        turno: 'P',
+    },
+    {
+        dia: '28',
+        matricula: '711045-6',
+        turno: 'P',
+    },
+    {
+        dia: '1',
+        matricula: '711045-6',
+        turno: 'P1',
+    },
+    {
+        dia: '1',
+        matricula: '707401-8',
         turno: 'P2',
     },
     {
-        dia: '5',
+        dia: '21',
         matricula: '707401-8',
+        turno: 'P2',
+    },
+    {
+        dia: '21',
+        matricula: '711045-6',
         turno: 'P1',
     },
     {
         dia: '5',
+        matricula: '725258-7',
+        turno: 'P1',
+    },
+    {
+        dia: '9',
+        matricula: '725258-7',
+        turno: 'P1',
+    },
+    {
+        dia: '5',
+        matricula: '718268-6',
+        turno: 'P2',
+    },
+    {
+        dia: '9',
+        matricula: '718268-6',
+        turno: 'P2',
+    },
+    {
+        dia: '8',
+        matricula: '725258-7',
+        turno: 'P',
+    },
+    {
+        dia: '13',
+        matricula: '725241-2',
+        turno: 'P1',
+    },
+    {
+        dia: '29',
+        matricula: '725241-2',
+        turno: 'P1',
+    },
+    {
+        dia: '26',
+        matricula: '707401-8',
+        turno: 'P1',
+    },
+    {
+        dia: '26',
+        matricula: '725262-5',
+        turno: 'P2',
+    },
+    {
+        dia: '10',
+        matricula: '707401-8',
+        turno: 'P1',
+    },
+    {
+        dia: '10',
+        matricula: '725262-5',
+        turno: 'P1',
+    },
+    {
+        dia: '20',
         matricula: '711045-6',
+        turno: 'P1',
+    },
+    {
+        dia: '20',
+        matricula: '718268-6',
+        turno: 'P2',
+    },
+    {
+        dia: '25',
+        matricula: '711045-6',
+        turno: 'P1',
+    },
+    {
+        dia: '25',
+        matricula: '718268-6',
+        turno: 'P2',
+    },
+    {
+        dia: '13',
+        matricula: '707401-8',
         turno: 'P2',
     },
     {
         dia: '29',
         matricula: '707401-8',
-        turno: 'P1',
-    },
-    {
-        dia: '29',
-        matricula: '711045-6',
-        turno: 'P2',
-    },
-    {
-        dia: '9',
-        matricula: '707401-8',
-        turno: 'P1',
-    },
-    {
-        dia: '9',
-        matricula: '711045-6',
         turno: 'P2',
     },
 ];
@@ -253,11 +303,6 @@ const pjesJulho: PJES[] = [
         turno: 'P',
     },
     {
-        dia: '20',
-        matricula: '711045-6',
-        turno: 'P',
-    },
-    {
         dia: '28',
         matricula: '711045-6',
         turno: 'P',
@@ -283,11 +328,6 @@ const pjesJulho: PJES[] = [
         turno: 'P1',
     },
     {
-        dia: '25',
-        matricula: '725262-5',
-        turno: 'P',
-    },
-    {
         dia: '5',
         matricula: '725258-7',
         turno: 'P1',
@@ -304,16 +344,6 @@ const pjesJulho: PJES[] = [
     },
     {
         dia: '9',
-        matricula: '718268-6',
-        turno: 'P2',
-    },
-    {
-        dia: '13',
-        matricula: '718268-6',
-        turno: 'P2',
-    },
-    {
-        dia: '29',
         matricula: '718268-6',
         turno: 'P2',
     },
@@ -333,14 +363,54 @@ const pjesJulho: PJES[] = [
         turno: 'P1',
     },
     {
-        dia: '10',
+        dia: '26',
         matricula: '707401-8',
-        turno: 'P',
+        turno: 'P1',
     },
     {
         dia: '26',
+        matricula: '725262-5',
+        turno: 'P2',
+    },
+    {
+        dia: '10',
         matricula: '707401-8',
-        turno: 'P',
+        turno: 'P1',
+    },
+    {
+        dia: '10',
+        matricula: '725262-5',
+        turno: 'P1',
+    },
+    {
+        dia: '20',
+        matricula: '711045-6',
+        turno: 'P1',
+    },
+    {
+        dia: '20',
+        matricula: '718268-6',
+        turno: 'P2',
+    },
+    {
+        dia: '25',
+        matricula: '711045-6',
+        turno: 'P1',
+    },
+    {
+        dia: '25',
+        matricula: '718268-6',
+        turno: 'P2',
+    },
+    {
+        dia: '13',
+        matricula: '707401-8',
+        turno: 'P2',
+    },
+    {
+        dia: '29',
+        matricula: '707401-8',
+        turno: 'P2',
     },
 ]; /* [
     {
