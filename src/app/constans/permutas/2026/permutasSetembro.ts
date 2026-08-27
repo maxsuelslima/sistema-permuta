ba7944a78ad4a2dc8ab6d7efedc811f8f92bfedb
Maxsuel Lima <maxsuelslima@gmail.com>
2026-08-27 17:59:04 -0300

    update.

--- src/app/constans/permutas/2026/permutasSetembro.ts
+++ src/app/constans/permutas/2026/permutasSetembro.ts
@@ -1,4 +1,239 @@
 import { Permuta } from '@/app/types/Permuta';
 
-const permutasSetembro: Array<Permuta> = [];
+const permutasSetembro: Array<Permuta> = [
+    {
+        id: '1787862837118',
+        servicos: [
+            {
+                dia: '1',
+                matricula: '725265-0',
+            },
+            {
+                dia: '7',
+                matricula: '726352-0',
+            },
+        ],
+    },
+    {
+        id: '1787862840078',
+        servicos: [
+            {
+                dia: '2',
+                matricula: '726388-0',
+            },
+            {
+                dia: '11',
+                matricula: '726352-0',
+            },
+        ],
+    },
+    {
+        id: '1787862848962',
+        servicos: [
+            {
+                dia: '5',
+                matricula: '725265-0',
+            },
+            {
+                dia: '8',
+                matricula: '725261-7',
+            },
+        ],
+    },
+    {
+        id: '1787862851864',
+        servicos: [
+            {
+                dia: '6',
+                matricula: '726388-0',
+            },
+            {
+                dia: '12',
+                matricula: '725261-7',
+            },
+        ],
+    },
+    {
+        id: '1787862864598',
+        servicos: [
+            {
+                dia: '13',
+                matricula: '725265-0',
+            },
+            {
+                dia: '10',
+                matricula: '726388-0',
+            },
+        ],
+    },
+    {
+        id: '1787862881513',
+        servicos: [
+            {
+                dia: '7',
+                matricula: '718268-6',
+            },
+            {
+                dia: '16',
+                matricula: '711045-6',
+            },
+        ],
+    },
+    {
+        id: '1787862888031',
+        servicos: [
+            {
+                dia: '11',
+                matricula: '718268-6',
+            },
+            {
+                dia: '20',
+                matricula: '711045-6',
+            },
+        ],
+    },
+    {
+        id: '1787862917958',
+        servicos: [
+            {
+                dia: '16',
+                matricula: '725261-7',
+            },
+            {
+                dia: '19',
+                matricula: '726352-0',
+            },
+        ],
+    },
+    {
+        id: '1787862925067',
+        servicos: [
+            {
+                dia: '17',
+                matricula: '725265-0',
+            },
+            {
+                dia: '23',
+                matricula: '726352-0',
+            },
+        ],
+    },
+    {
+        id: '1787862929099',
+        servicos: [
+            {
+                dia: '18',
+                matricula: '726388-0',
+            },
+            {
+                dia: '27',
+                matricula: '726352-0',
+            },
+        ],
+    },
+    {
+        id: '1787862935212',
+        servicos: [
+            {
+                dia: '21',
+                matricula: '725265-0',
+            },
+            {
+                dia: '24',
+                matricula: '725261-7',
+            },
+        ],
+    },
+    {
+        id: '1787862951144',
+        servicos: [
+            {
+                dia: '28',
+                matricula: '725261-7',
+            },
+            {
+                dia: '22',
+                matricula: '726388-0',
+            },
+        ],
+    },
+    {
+        id: '1787862954302',
+        servicos: [
+            {
+                dia: '29',
+                matricula: '725265-0',
+            },
+            {
+                dia: '26',
+                matricula: '726388-0',
+            },
+        ],
+    },
+    {
+        id: '1787863004433',
+        servicos: [
+            {
+                dia: '27',
+                matricula: '718268-6',
+            },
+            {
+                dia: '17',
+                matricula: '711014-6',
+            },
+        ],
+    },
+    {
+        id: '1787863014932',
+        servicos: [
+            {
+                dia: '23',
+                matricula: '718268-6',
+            },
+            {
+                dia: '21',
+                matricula: '711014-6',
+            },
+        ],
+    },
+    {
+        id: '1787863034927',
+        servicos: [
+            {
+                dia: '24',
+                matricula: '711045-6',
+            },
+            {
+                dia: '29',
+                matricula: '711014-6',
+            },
+        ],
+    },
+    {
+        id: '1787863847719',
+        servicos: [
+            {
+                dia: '1',
+                matricula: '711014-6',
+            },
+            {
+                dia: '4',
+                matricula: '711045-6',
+            },
+        ],
+    },
+    {
+        id: '1787863858173',
+        servicos: [
+            {
+                dia: '9',
+                matricula: '711014-6',
+            },
+            {
+                dia: '12',
+                matricula: '711045-6',
+            },
+        ],
+    },
+];
 export default permutasSetembro;
